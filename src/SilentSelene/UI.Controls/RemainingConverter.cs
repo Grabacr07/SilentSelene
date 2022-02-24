ff3c9c8409eed0126f1f2da23b78bc81c808e864
Manato KAMEYA <grabacr07+github@gmail.com>
2022-02-24 21:02:23 +0900

    Minor changes.

---
+++ src/SilentSelene/UI.Controls/RemainingConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace SilentSelene.UI.Controls;
+
+public class RemainingConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is TimeSpan span
+            ? $"{span.TotalHours:00}:{span.Minutes:00}:{span.Seconds:00}"
+            : "";
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
